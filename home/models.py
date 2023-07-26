075f4d4b2baebf3b01a29b5ce1172c851e3ca6e8
mseo39 <a01063294905@gmail.com>
2023-07-26 15:00:59 +0900

    mysql

--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    UniqueNumber = models.CharField(max_length=12, primary_key=True)
+    Name = models.CharField(max_length=3)
+    ResidentRegistration = models.CharField(max_length=13)
+    ResidentialArea = models.CharField(max_length=13)
+    PhoneNumbe = models.IntegerField
+    MenberID = models.CharField(max_length=6)
+    password = models.CharField(max_length=20)
+    Age = models.IntegerField
+    CarNumber = models.IntegerField
+    BankAccount = models.CharField(max_length=16)
+
+class CarList(models.Model):
+    status=(
+        ('T', 'true'),
+        ('F','false')
+    )
+    Date = models.DateTimeField
+    Latitude= models.DecimalField(max_digits=18, decimal_places=10)
+    Longitube= models.DecimalField(max_digits=18, decimal_places=10)
+    CarNum = models.CharField(max_length=8)
+    UniqueNumber=models.ForeignKey(Member, related_name="carlist_UniqueNumber", on_delete=models.CASCADE)
+    MenberID = models.CharField(max_length=6)
+    ReportStatus = models.CharField(max_length=1, choices=status)
+
+class ReportList(models.Model):
+    BeforeDate= models.DateTimeField
+    AfterDate = models.DateTimeField
+    CarNum = models.CharField(max_length=8)
+    BeforeUniqueNumber=models.ForeignKey(Member, related_name="before_UniqueNumber", on_delete=models.CASCADE)
+    AfterUniqueNumber=models.ForeignKey(Member, related_name="after_UniqueNumber", on_delete=models.CASCADE)
--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql 
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '3m',
+        'USER': 'root',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
