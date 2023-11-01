2ebacc5a40b115adde8dd1da7440a11ff0ae2960
mseo39 <a01063294905@gmail.com>
2023-11-01 18:29:37 +0900

    add fcm_token

--- send/views.py
+++ send/views.py
@@ -19,7 +19,7 @@ def Data(request):
             #다음 코드는 똑같은 값이 여러개 있을 때 에러가 남
             #recent_5min=CarList.objects.get(CarNum=request.data["CarNum"])
             recent_5min = CarList.objects.filter(CarNum=request.data["CarNum"], ReportStatus='F').order_by('-Date').first()
-            if recent_5min and datetime.strptime(request.data["Date"], '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.UTC)-recent_5min.Date >= timedelta(minutes=5):
+            if recent_5min and datetime.strptime(request.data["Date"], '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.UTC)-recent_5min.Date >= timedelta(minutes=1):
                 
                 # 여기에 위도, 경도 비교 기능 작성
 
@@ -48,6 +48,8 @@ def Data(request):
                     serializer = ReportListSerializers(data=data)
                     if serializer.is_valid():
                         serializer.save() # ReportList에 저장
+                        get_fcm_token_by_member_id(recent_5min.MemberID)
+                        get_fcm_token_by_member_id(request.data["MemberID"])
                     else:
                         print(serializer.errors)
                         return JsonResponse({'message': 'ReportListSerializer error', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
@@ -68,3 +70,33 @@ def Data(request):
             return JsonResponse({'message': 'serializer error', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         return JsonResponse({'message':"successfully"}, status=status.HTTP_200_OK)
     return JsonResponse({'message': 'error'}, status=status.HTTP_400_BAD_REQUEST)
+
+from firebase_admin import messaging
+
+def get_fcm_token_by_member_id(member_id):
+    try:
+        member = Member.objects.get(MemberID=member_id)
+        fcm_token = member.fcm_token
+        send_to_firebase_cloud_messaging(fcm_token)
+    except Member.DoesNotExist:
+        return None  # 멤버를 찾지 못한 경우 None을 반환
+
+
+#안드로이드로 알림 보내기
+def send_to_firebase_cloud_messaging(token):
+    # This registration token comes from the client FCM SDKs.
+    registration_token = token
+    print(registration_token)
+
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+    notification=messaging.Notification(
+        title='불법 주정차 차량이 발견됐어요!',
+        body='내역을 확인하고 신고를 접수해주세요.',
+    ),
+    token=registration_token,
+    )
+
+    response = messaging.send(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
